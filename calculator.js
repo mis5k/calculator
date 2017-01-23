6119319202d1807ef24cc71871451ae64b1758db
mis5k <muck_hyang@naver.com>
2017-01-23 10:54:37 +0900

    first commit

---
+++ calculator.js
@@ -0,0 +1,100 @@
+var Calculator = (function () {
+    function Calculator() {
+    }
+    Calculator.prototype.init = function () {
+        this.exp = [];
+        this.stack = [];
+    };
+    Calculator.prototype.getStackTop = function () {
+        return this.stack[this.stack.length - 1];
+    };
+    Calculator.prototype.isStackEmpry = function () {
+        return true;
+    };
+    Calculator.prototype.isOperator = function (str) {
+        return (str == '+') || (str == '-') || (str == '*') || (str == '/');
+    };
+    Calculator.prototype.precedence = function (op) {
+        if (op == '+' || op == '-') {
+            return 0;
+        }
+        else if (op == '*' || op == '/') {
+            return 1;
+        }
+        else {
+            return 2;
+        }
+    };
+    Calculator.prototype.postfix = function (input) {
+        this.init();
+        for (var i = 0; i < input.length; i++) {
+            if (this.isOperator(input[i])) {
+                if (this.stack.length > 0
+                    && (this.precedence(this.getStackTop()) >= this.precedence(input[i]))) {
+                    while (this.stack.length > 0) {
+                        this.exp.push(this.stack.pop());
+                    }
+                }
+                this.stack.push(input[i]);
+            }
+            else if (input[i] >= '0' && input[i] <= '9') {
+                this.exp.push(input[i]);
+            }
+        }
+        if (this.stack.length > 0) {
+            while (this.stack.length > 0) {
+                this.exp.push(this.stack.pop());
+            }
+        }
+    };
+    Calculator.prototype.evaluateOperator = function (op, left, right) {
+        switch (op) {
+            case "+": return left + right;
+            case "-": return left - right;
+            case "*": return left * right;
+            case "/": return left / right;
+        }
+    };
+    Calculator.prototype.calc = function () {
+        this.stack = [];
+        for (var i = 0; i < this.exp.length; i++) {
+            if (this.exp[i] >= '0' && this.exp[i] <= '9') {
+                this.stack.push(this.exp[i]);
+            }
+            else if (this.isOperator(this.exp[i])) {
+                var right = Number(this.stack.pop());
+                var left = Number(this.stack.pop());
+                var val = this.evaluateOperator(this.exp[i], left, right);
+                this.stack.push(String(val));
+            }
+        }
+    };
+    Calculator.prototype.getResult = function (str) {
+        this.postfix(str);
+        this.calc();
+        return this.stack.pop();
+    };
+    return Calculator;
+}());
+var exp_temp = "";
+function clickBtn(input) {
+    console.log("A");
+    exp_temp += input;
+    document.getElementById("mainExp").innerHTML = exp_temp;
+}
+function getResult() {
+    console.log("getResult");
+    var c = new Calculator();
+    var result = c.getResult(exp_temp);
+    console.log("result : " + result);
+    exp_temp = result;
+    document.getElementById("mainExp").innerHTML = exp_temp;
+}
+function reset() {
+    console.log("reset");
+    exp_temp = "";
+    document.getElementById("mainExp").innerHTML = exp_temp;
+}
+//let c:Calculator = new Calculator();
+//let result = c.getResult("2*3+6/2-4");
+//console.log(result); 
---
+++ calculator.ts
@@ -0,0 +1,101 @@
+
+class Calculator {
+    private exp: string[];
+    private stack: string[];
+    
+    public init() {
+        this.exp = [];
+        this.stack = [];   
+    }
+    public getStackTop(): string {
+        return this.stack[this.stack.length - 1];
+    }
+    public isStackEmpry():boolean {
+        return true;
+    }
+    public isOperator(str: string): boolean {
+        return (str == '+') || (str == '-') || (str == '*') || (str == '/')
+    }
+    public precedence(op: string): number {
+        if(op == '+' || op == '-') {
+           return 0;
+        } else if(op == '*' || op == '/' ) {
+           return 1;
+        } else {
+           return 2;
+        }
+    }
+    public postfix(input: string) {
+        this.init();
+
+        for(let i=0; i<input.length; i++) {
+           if(this.isOperator(input[i])) {
+              if(this.stack.length > 0 
+                  && (this.precedence(this.getStackTop()) >= this.precedence(input[i]))) {
+                 while(this.stack.length > 0) {
+                    this.exp.push(this.stack.pop());
+                 }
+              }
+              this.stack.push(input[i]);
+           } else if(input[i] >= '0' && input[i] <= '9') {
+              this.exp.push(input[i]);
+           }
+        } 
+        if(this.stack.length > 0) {
+            while(this.stack.length > 0) {
+               this.exp.push(this.stack.pop());
+            }
+        }
+    }
+    public evaluateOperator(op: string, left: number, right: number): number {
+        switch (op) {
+            case "+": return left + right;
+            case "-": return left - right;
+            case "*": return left * right;
+            case "/": return left / right;
+        }
+    }
+    public calc() {
+        this.stack = [];
+        for(let i=0; i<this.exp.length; i++) {
+           if(this.exp[i] >= '0' && this.exp[i] <= '9') {
+               this.stack.push(this.exp[i]);
+           } else if(this.isOperator(this.exp[i])) {
+               let right:number = Number(this.stack.pop());
+               let left:number = Number(this.stack.pop());
+               let val:number = this.evaluateOperator(this.exp[i], left, right);
+               this.stack.push(String(val));
+           }
+        }        
+    }
+    public getResult(str: string): string {
+        this.postfix(str);
+        this.calc();
+        return this.stack.pop();
+    }
+}
+let exp_temp:string = "";
+
+function clickBtn(input:string) {
+    console.log("A");
+    exp_temp += input;
+    document.getElementById("mainExp").innerHTML = exp_temp; 
+}
+
+function getResult() {
+    console.log("getResult");
+    let c = new Calculator();
+    let result = c.getResult(exp_temp);
+    console.log("result : " + result);
+    exp_temp = result;
+    document.getElementById("mainExp").innerHTML = exp_temp;
+}
+function reset() {
+    console.log("reset");
+    exp_temp = "";        
+    document.getElementById("mainExp").innerHTML = exp_temp; 
+}
+
+//let c:Calculator = new Calculator();
+//let result = c.getResult("2*3+6/2-4");
+//console.log(result);
